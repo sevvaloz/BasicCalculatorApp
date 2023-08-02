51a0b10ff05eefd9092c2c31343e00f99cbcf353
sevvaloz <ozdamarsevval.01@gmail.com>
2023-08-02 12:42:15 +0300

    basic calculator app created.

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -8,12 +8,74 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    var result: Float = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    @IBAction func addition(_ sender: Any) {
+        if let numberOne = Float(firstNumber.text!){
+            if let numberTwo = Float(secondNumber.text!){
+                result = numberOne + numberTwo
+                resultLabel.text = String(result)
+            } else {
+                resultLabel.text = "Please give me a NUMBER"
+            }
+        } else {
+            resultLabel.text = "Please give me a NUMBER"
+        }
+    }
+    
 
-
+    @IBAction func subtraction(_ sender: Any) {
+        if let numberOne = Float(firstNumber.text!){
+            if let numberTwo = Float(secondNumber.text!){
+                result = numberOne - numberTwo
+                resultLabel.text = String(result)
+            } else {
+                resultLabel.text = "Please give me a NUMBER"
+            }
+        } else {
+            resultLabel.text = "Please give me a NUMBER"
+        }
+    }
+    
+    
+    @IBAction func multiplication(_ sender: Any) {
+        if let numberOne = Float(firstNumber.text!){
+            if let numberTwo = Float(secondNumber.text!){
+                result = numberOne * numberTwo
+                resultLabel.text = String(result)
+            } else {
+                resultLabel.text = "Please give me a NUMBER"
+            }
+        } else {
+            resultLabel.text = "Please give me a NUMBER"
+        }
+    }
+    
+    
+    @IBAction func division(_ sender: Any) {
+        if let numberOne = Float(firstNumber.text!){
+            if let numberTwo = Float(secondNumber.text!){
+                result = numberOne / numberTwo
+                resultLabel.text = String(result)
+            } else {
+                resultLabel.text = "Please give me a NUMBER"
+            }
+        } else {
+            resultLabel.text = "Please give me a NUMBER"
+        }
+    }
+    
+    
 }
 
